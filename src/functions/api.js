6b53d17b2a76b3ea3220b25fabf42a17b7d0ac3c
insodev-dev0 <cesar.latorre@insodev.cl>
2023-08-03 16:59:56 -0400

    Axios

--- src/functions/api.js
+++ src/functions/api.js
@@ -1,17 +1,16 @@
 import axios from "axios";
 
 export const getApiServers = () => {
-  return axios.get(
-    "https://script.google.com/macros/s/AKfycbxmhLmVuwNtMfp_t4gGqQrtQEIdtBhL3rvFMC2ja_U8LScXMdPIiASp17Cq8mwcYYpS-Q/exec"
-  );
+    return axios.get(
+        "https://script.google.com/macros/s/AKfycbxmhLmVuwNtMfp_t4gGqQrtQEIdtBhL3rvFMC2ja_U8LScXMdPIiASp17Cq8mwcYYpS-Q/exec"
+    );
 };
 
 export const getApiLogin = (server, username, password) => {
-  return axios.get(
-    `https://${server}/api/login/?username=${username}&password=${password}`
-  );
+    return axios.get(
+        `https://${server}/api/login/?username=${username}&password=${password}`
+    );
 };
-
 
 export const getApiData = (server, empresa_id) => {
     return axios.get(`https://${server}/api/app/data/${empresa_id}/`);
